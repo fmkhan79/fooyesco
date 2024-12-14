e28c35b9d128e18e4a23b3d7e9a4dcfbd2f03c6b
Ahmed Ali <website25developer@gmail.com>
2024-12-14 16:17:57 +0500

    Update of index.php
    Update of index.php mob design

--- application/views/frontend/default/restaurant/index.php
+++ application/views/frontend/default/restaurant/index.php
@@ -16,13 +16,11 @@
 
 
 
-/* Show the element only on mobile screens (<= 768px) */
-/* @media (max-width: 768px) {
+    /* Show the element only on mobile screens (<= 768px) */
+    /* @media (max-width: 768px) {
     #mobile-only {
         display: block;
     } */
-
-
 </style>
 <!-- RESTAURANT GALLERY -->
 
@@ -181,84 +179,249 @@ $ctaLink = $this->order_model->getSetting('ctaLink');
 <!-- ./Category tabs with scrool nav -->
 
 
- <div class="container mt-5">
+<div class="container mt-5">
     <div class="accordion" id="accordionExample">
-      <div class="accordion-item d-md-none">
-      <!-- <div class="accordion-item"> -->
+        <div class="accordion-item d-md-none">
+            <!-- <div class="accordion-item"> -->
 
-      <h2 class="accordion-header" id="headingOne">
-        <button class="accordion-button order-red-btn text-center mt-4 border-0 w-100" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-    <?php echo site_phrase('cart', true); ?>
-</button>
+            <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button order-red-btn text-center mt-4 border-0 w-100" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                    <?php echo site_phrase('cart', true); ?>
+                </button>
 
-        </h2>
-        <!-- Initially hidden content -->
-        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-          <div class="accordion-body">
+            </h2>
+            <!-- Initially hidden content -->
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
 
-          <div id="item-list">
+                    <div id="item-list">
 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
-          
-                 
+<div class="col-md-6 mt-3 order-summery-box d-md-none" id="">
+    <h3 class="d-flex justify-content-center">Order Summary</h3>
+    <p class="green d-flex justify-content-center">You're all set</p>
 
-        
+    <div id="item-list">
+
+    </div>
+
+    <div
+        class="c-basketSwitcher u-spacingBottom d-flex align-items-center justify-content-center my-4 mx-5">
+        <label class="c-basketSwitcher-switch d-flex align-items-center justify-content-between mr-4">
+            <input type="radio" name="basket-switcher" value="delivery" checked="checked"
+                class="is-visuallyHidden"> <svg xmlns="http://www.w3.org/2000/svg" width="24"
+                height="24" viewBox="0 0 24 24" fill="none">
+                <g clip-path="url(#clip0_4609_18686)">
+                    <path
+                        d="M5 21C6.65685 21 8 19.6569 8 18C8 16.3431 6.65685 15 5 15C3.34315 15 2 16.3431 2 18C2 19.6569 3.34315 21 5 21Z"
+                        stroke="black" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path
+                        d="M19 21C20.6569 21 22 19.6569 22 18C22 16.3431 20.6569 15 19 15C17.3431 15 16 16.3431 16 18C16 19.6569 17.3431 21 19 21Z"
+                        stroke="black" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M12 19V15L9 12L14 8L16 11H19" stroke="black" stroke-width="2"
+                        stroke-linecap="round" stroke-linejoin="round" />
+                    <path
+                        d="M17 6C17.5523 6 18 5.55228 18 5C18 4.44772 17.5523 4 17 4C16.4477 4 16 4.44772 16 5C16 5.55228 16.4477 6 17 6Z"
+                        stroke="black" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                </g>
+                <defs>
+                    <clippath id="clip0_4609_18686">
+                        <rect width="24" height="24" fill="white" />
+                    </clippath>
+                </defs>
+            </svg>
+            <div class="c-basketSwitcher-eta-wrapper"><span>
+                    Delivery<br />
+                </span> <span class="c-basketSwitcher-eta">
+                    from 7:35
+                </span></div>
+        </label> <label
+            class="c-basketSwitcher-switch d-flex align-items-center justify-content-between"><input
+                type="radio" name="basket-switcher" value="collection" class="is-visuallyHidden">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24"
+                viewBox="0 0 25 24" fill="none">
+                <g clip-path="url(#clip0_4609_18694)">
+                    <path
+                        d="M18 20C19.3807 20 20.5 18.8807 20.5 17.5C20.5 16.1193 19.3807 15 18 15C16.6193 15 15.5 16.1193 15.5 17.5C15.5 18.8807 16.6193 20 18 20Z"
+                        stroke="#F2F2F2" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path
+                        d="M6.5 8V19C6.50019 19.2107 6.56691 19.4159 6.69063 19.5864C6.81436 19.7569 6.98877 19.884 7.18899 19.9495C7.3892 20.015 7.605 20.0156 7.80558 19.9513C8.00617 19.8869 8.1813 19.7608 8.306 19.591L12 14.5V14.555"
+                        stroke="#F2F2F2" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path
+                        d="M6.5 8H21.5L18 15L10.9 14.253C10.1757 14.1769 9.48595 13.9045 8.90511 13.4651C8.32427 13.0258 7.87439 12.4362 7.604 11.76L4.751 4.63C4.67702 4.44425 4.54906 4.28494 4.38364 4.17264C4.21822 4.06034 4.02294 4.00021 3.823 4H2.5"
+                        stroke="#F2F2F2" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                </g>
+                <defs>
+                    <clippath id="clip0_4609_18694">
+                        <rect width="24" height="24" fill="white"
+                            transform="translate(0.5)" />
+                    </clippath>
+                </defs>
+            </svg>
+            <div class="c-basketSwitcher-eta-wrapper"><span>
+                    Collection<br />
+                    15 Minutes
+                </span></div>
+        </label>
+    </div>
+
+    <!-- start -->
+
+
+    <?php
+    $cart_items = $this->cart_model->get_cart_by_condition(['customer_id' => $this->session->userdata('user_id'), 'restaurant_id' => sanitize($restaurant_details['id'])]);
+    ?>
+
+    <hr />
+    <?php if (sizeof($cart_items) > 0) { ?>
+        <div class="row d-flex align-items-center justify-content-center mx-5">
+            <div class="col-sm-12">
+                <div class="form-group">
+                    <label for="promo_code">Promo Code</label>
+                    <div class="d-flex gap-2 justify-content-center">
+                        <input type="text" class="form-control" id="promo_code" name="promo_code"
+                            value="<?php echo $cart_items[0]['offer_code']; ?>" required>
+                        <?php if (isset($cart_items[0]['offer_code'])) { ?>
+                            <div class="btn btn-sm btn-danger m-2" onclick="remove_promo()">
+                                <i class="fa fa-times"></i>
+                            </div>
+                        <?php } ?>
+                        <div class="btn btn-sm btn-danger m-2" id="remove_promo" style="display:none"
+                            onclick="remove_promo()">
+                            <i class="fa fa-times"></i>
+                        </div>
+                    </div>
+                    <small id="promo_code_message"></small> <!-- Container for messages -->
+                    <?php if (isset($cart_items[0]['offer_code'])) { ?>
+                        <small class="text-success">Promo is already applied.</small>
+                        <!-- Container for messages -->
+                    <?php } else { ?>
+                        <div class="btn btn-sm btn-warning w-100 mt-2 text-dark" id="apply_promo"
+                            onclick="apply_promo_action()">APPLY COUPON CODE
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
         </div>
+    <?php } ?>
+
+    <!-- <div class="offer-spend my-3">Offer Spend £28.05 more to get 10% off</div> -->
+
+    <div class="day-time-box my-4 p-2 mx-3">
+
+        <div class="d-flex align-items-center justify-content-between mb-2">
+            Day <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                viewBox="0 0 24 24" fill="none">
+                <g clip-path="url(#clip0_4609_18708)">
+                    <path
+                        d="M11.795 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V7C3 6.46957 3.21071 5.96086 3.58579 5.58579C3.96086 5.21071 4.46957 5 5 5H17C17.5304 5 18.0391 5.21071 18.4142 5.58579C18.7893 5.96086 19 6.46957 19 7V11"
+                        stroke="#F54748" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path
+                        d="M18 22C20.2091 22 22 20.2091 22 18C22 15.7909 20.2091 14 18 14C15.7909 14 14 15.7909 14 18C14 20.2091 15.7909 22 18 22Z"
+                        stroke="#F54748" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M15 3V7" stroke="#F54748" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M7 3V7" stroke="#F54748" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M3 11H19" stroke="#F54748" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M18 16.4961V18.0001L19 19.0001" stroke="#F54748" stroke-width="2"
+                        stroke-linecap="round" stroke-linejoin="round" />
+                </g>
+                <defs>
+                    <clippath id="clip0_4609_18708">
+                        <rect width="24" height="24" fill="white" />
+                    </clippath>
+                </defs>
+            </svg>
         </div>
-        
-      </div>
-    
+
+        <div class="d-flex align-items-center justify-content-between">
+            Time <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                viewBox="0 0 24 24" fill="none">
+                <g clip-path="url(#clip0_4609_18717)">
+                    <path
+                        d="M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21Z"
+                        stroke="#F54748" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M12 12L15 10" stroke="#F54748" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M12 7V12" stroke="#F54748" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                </g>
+                <defs>
+                    <clippath id="clip0_4609_18717">
+                        <rect width="24" height="24" fill="white" />
+                    </clippath>
+                </defs>
+            </svg>
+        </div>
+
+        <!-- end -->
+
+        <div class="total-price-box d-flex justify-content-between align-items-center">
+            <div class="subtotal">Subtotal</div>
+            <div id="ttprice" class="subtotal-price"></div>
+        </div>
+
+        <div class="total-price-box d-flex justify-content-between align-items-center">
+            <div class="subtotal">Delivery Charges</div>
+            <div class="total-delivery-price"></div>
+        </div>
+
+        <div class="total-price-box d-flex justify-content-between align-items-center">
+            <div class="subtotal">VAT Charges</div>
+            <div class="total-vat-price"></div>
+        </div>
+
+        <div class="total-price-box d-flex justify-content-between align-items-center">
+            <div class="subtotal">Service Charges</div>
+            <div class="total-service-price"></div>
+        </div>
+
+
+        <?php $customer_details = $this->customer_model->get_by_id($this->session->userdata('user_id')); ?>
+        <?php if (empty($customer_details) || empty($customer_details['name'])) : ?>
+            <!-- Login Button -->
+            <div class="button-container d-flex justify-content-between mt-2">
+
+                <button id="mobile-only" class="order-red-btn text-center mt-4 border-0 w-50" onclick="window.location.href='<?php echo site_url('auth'); ?>'">
+                    Login
+                </button>
+
+                <!-- Sign Up Button -->
+
+                <a href="<?php echo site_url('GuestCheckout'); ?>" class="order-red-btn text-center mt-4 disabled border-0" id="guestCheckoutBtnmobile">
+                    Guest Checkout
+                </a>
+
+                <!-- Guest Checkout Button -->
+
+            <?php else : ?>
+                <!-- Checkout Button -->
+                <button class="order-red-btn text-center mt-4 w-100 border-0 disabled" id="CheckoutBtnmobile" style="cursor: pointer;" disabled>
+                    <?php echo site_phrase('checkout', true); ?>
+                </button>
+            <?php endif; ?>
+            </div>
     </div>
 
 
-    <div class="d-md-none">
-
-    <div class="total-price-box d-flex justify-content-between align-items-center">
-                        <div class="subtotal">Subtotal</div>
-                        <div id="ttprice" class="subtotal-price"></div>
-                    </div>
-
-                    <div class="total-price-box d-flex justify-content-between align-items-center">
-                        <div class="subtotal">Delivery Charges</div>
-                        <div class="total-delivery-price"></div>
-                    </div>
-
-                    <div class="total-price-box d-flex justify-content-between align-items-center">
-                        <div class="subtotal">VAT Charges</div>
-                        <div class="total-vat-price"></div>
-                    </div>
-
-                    <div class="total-price-box d-flex justify-content-between align-items-center">
-                        <div class="subtotal">Service Charges</div>
-                        <div class="total-service-price"></div>
-                    </div>
-                    
-                  
-    <?php $customer_details = $this->customer_model->get_by_id($this->session->userdata('user_id')); ?>
-         <?php if (empty($customer_details) || empty($customer_details['name'])) : ?>
-            <!-- Login Button -->
-            <button id="mobile-only" class="order-red-btn text-center mt-4 border-0" onclick="window.location.href='<?php echo site_url('auth'); ?>'">
-            Login 
-            </button>
-            
-            <!-- Sign Up Button -->
-         
-            <a href="<?php echo site_url('GuestCheckout'); ?>" class="order-red-btn text-center mt-4 disabled border-0" id="guestCheckoutBtnmobile">
-                                Guest Checkout
-                            </a>
-            
-            <!-- Guest Checkout Button -->
-            
-            <?php else : ?>
-            <!-- Checkout Button -->
-            <button class="order-red-btn text-center mt-4 w-100 border-0 disabled" id="CheckoutBtnmobile" style="cursor: pointer;" disabled>
-                <?php echo site_phrase('checkout', true); ?>
-            </button>
-        <?php endif; ?>
-        </div>
-  </div>
-
+</div>
 
 <!-- Menu section -->
 
@@ -465,11 +628,11 @@ $ctaLink = $this->order_model->getSetting('ctaLink');
                                 </span></div>
                         </label>
                     </div>
-                   
+
 
                     <div class="total-price-box d-flex justify-content-between align-items-center">
                         <div class="subtotal">Subtotal</div>
-                        
+
                         <div id="ttprice" class="subtotal-price"></div>
                     </div>
 
@@ -601,15 +764,15 @@ $ctaLink = $this->order_model->getSetting('ctaLink');
                             <a href="<?php echo site_url('auth'); ?>" class="d-block order-red-btn text-center mt-4">
                                 <?php echo site_phrase('login_first', true); ?>
                             </a>
-                            
-                            <a href="<?php echo site_url('auth/registration/customer'); ?>"  class="d-block order-red-btn text-center mt-4">
+
+                            <a href="<?php echo site_url('auth/registration/customer'); ?>" class="d-block order-red-btn text-center mt-4">
                                 Sign UP
                             </a>
                             <a href="<?php echo site_url('GuestCheckout'); ?>" id="guestCheckoutBtn" class="d-block order-red-btn text-center mt-4 disabled">
                                 Guest Checkout
                             </a>
-                          
-                          
+
+
                         <?php else : ?>
                             <a href="<?php echo site_url('checkout'); ?>" class="d-block order-red-btn text-center mt-4 w-100 border-0 disabled" id="CheckoutBtn" style="cursor: pointer;" role="button">
                                 <?php echo site_phrase('checkout', true); ?>
