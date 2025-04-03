c424f7fc8d57591213a5af13ecc942bafce46446
mutayyab09 <website25developer@gmail.com>
2025-04-03 15:40:38 +0500

    terms & privacy

--- application/views/frontend/default/terms_and_conditions/index.php
+++ application/views/frontend/default/terms_and_conditions/index.php
@@ -5,51 +5,105 @@
 <section class="detail-wbox mt-4 mb-2">
     <div class="container bg-white text-dark border border-light">
 
-        <h4 class="mt-5 mb-5 text-dark"><?php echo site_phrase('terms_and_conditions', true) ?></h4>
+        <h2 class="mt-5 mb-5 text-dark"><?php echo site_phrase('terms_and_conditions', true) ?></h2>
         <div class="about-bot-txt my-5">
-            <p>This is a type of resturent which typically serves food and drink, in addition to light refreshments such
-                as baked goods or snacks. The term comes frome the rench word meaning food Are you hungry? Did you have
-                a long and stressful day? Interested in getting a cheesy pizza delivered to your office or looking to
-                avoid the weekly shop? Then Pakistan is the right destination for you! offers you a long and detailed
-                list of the best restaurants and shops near you to help make your everyday easier. Our online food
-                delivery service has it all, whether you fancy Indian, Pakistani or Afghan cuisine, Pakistan has over
-                15,000 restaurants available in top cities like Islamabad, Lahore, Rawalpindi, and Karachi. Did you know
-                you can order your groceries and more from , too? Check out shops for favourite partners like Al-Fatah,
-                Greenvalley and more. Sit back and relax, let Pakistan take the pressure off your shoulders.</p>
-            <p>It is a long established fact that a reader will be distracted by the readable content of a page when
-                looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution
-                of letters, as opposed to using 'Content here, content here', making it look like readable English. Many
-                desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a
-                search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have
-                evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).It is
-                a long established fact that a reader will be distracted by the readable content of a page when looking
-                at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of
-                letters, as opposed to using 'Content here, content here', making it look like readable English. Many
-                desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a
-                search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have
-                evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-            <p>This is a type of resturent which typically serves food and drink, in addition to light refreshments such
-                as baked goods or snacks. The term comes frome the rench word meaning food Are you hungry? Did you have
-                a long and stressful day? Interested in getting a cheesy pizza delivered to your office or looking to
-                avoid the weekly shop? Then foodpanda Pakistan is the right destination for you! foodpanda offers you a
-                long and detailed list of the best restaurants and shops near you to help make your everyday easier. Our
-                online food delivery service has it all, whether you fancy Indian, Pakistani or Afghan cuisine,
-                foodpanda Pakistan has over 15,000 restaurants available in top cities like Islamabad, Lahore,
-                Rawalpindi, and Karachi. Did you know you can order your groceries and more from foodpanda, too? Check
-                out foodpanda shops for favourite partners like Al-Fatah, Greenvalley and more. Sit back and relax – let
-                foodpanda Pakistan take the pressure off your shoulders.</p>
-            <p>It is a long established fact that a reader will be distracted by the readable content of a page when
-                looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution
-                of letters, as opposed to using 'Content here, content here', making it look like readable English. Many
-                desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a
-                search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have
-                evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).It is
-                a long established fact that a reader will be distracted by the readable content of a page when looking
-                at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of
-                letters, as opposed to using 'Content here, content here', making it look like readable English. Many
-                desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a
-                search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have
-                evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+            <p>Welcome to Fooyes UK! By accessing and using our website, you agree to comply with the following Terms and Conditions. Please read them carefully before using our services.
+            </p>
+            <br>
+            <span><h3 style="display: inline;">1. General Information</h3></span>
+            <br>
+            <br>
+            <div class="about-bot-txt">
+        <ul style="gap: 10px; display: grid;">
+            <li>These Terms and Conditions govern your use of the Fooyes website and services. By using our platform, you acknowledge and accept these terms in full.</li>
+        </ul>
+            <span><h3 style="display: inline;">2. Ordering and Payments</h3></span>
+            <br>
+            <br>
+            <div class="about-bot-txt">
+        <ul style="gap: 10px; display: grid;">
+            <li>All orders placed through our website are subject to availability.            </li>
+            <li>Prices listed are in GBP and may be subject to change without prior notice.</li>
+            <li> Payment must be made at the time of purchase using our accepted payment methods.</li>
+            <li>We reserve the right to cancel or refuse orders at our discretion.
+            </li>
+        </ul>
+            <span><h3 style="display: inline;">3. Delivery and Shipping</h3></span>
+            <br>
+            <br>
+            <div class="about-bot-txt">
+        <ul style="gap: 10px; display: grid;">
+            <li>Delivery times are estimated and may vary due to unforeseen circumstances.
+            </li>
+            <li>Customers must provide accurate delivery information. Fooyes UK is not responsible for orders lost due to incorrect details.</li>
+        </ul>
+        <span><h3 style="display: inline;">4. Order Cancellation and Amendments</h3></span>
+            <br>
+            <br>
+            <div class="about-bot-txt">
+        <ul style="gap: 10px; display: grid;">
+            <li>Once the order has been confirmed and payment taken from your account, you will be unable to cancel your order and will not be eligible for a refund.</li>
+            <li>To change or cancel your order please contact the Fooyes Customer Support team who will attempt to resolve your request.</li>
+            <li> Fooyes will contact the Takeaway on your behalf but cannot guarantee your request will be accepted as food processing may already be underway.</li>
+        </ul>
+        <span><h3 style="display: inline;">5. Compensation</h3></span>
+            <br>
+            <br>
+            <div class="about-bot-txt">
+        <ul style="gap: 10px; display: grid;">
+            <li>If you are unhappy with the quality of any goods or the service provided by the Takeaway and wish to seek any form of compensation, you should contact the Takeaway directly to raise your complaint and, where appropriate, follow the Takeaway's own complaint procedures.</li>
+        </ul>
+        <span><h3 style="display: inline;">6. Refunds</h3></span>
+            <br>
+            <br>
+            <div class="about-bot-txt">
+        <ul style="gap: 10px; display: grid;">
+            <li> Refunds will be processed within a reasonable timeframe upon approval.</li>
+        </ul>
+        <span><h3 style="display: inline;">7. Use of Our Website        </h3></span>
+            <br>
+            <br>
+            <div class="about-bot-txt">
+        <ul style="gap: 10px; display: grid;">
+            <li>You agree not to misuse our website for fraudulent activities or unlawful purposes.</li>
+            <li>Unauthorized use of the website may result in legal action.</li>
+            <li>We reserve the right to update or modify our website without prior notice.</li>
+        </ul>
+        <span><h3 style="display: inline;">8. Intellectual Property</h3></span>
+            <br>
+            <br>
+            <div class="about-bot-txt">
+        <ul style="gap: 10px; display: grid;">
+            <li>All content on the Fooyes UK website, including images, text, and logos, is our property and protected by copyright laws.</li>
+            <li> You may not reproduce, distribute, or use any content without prior written consent.</li>
+        </ul>
+        <span><h3 style="display: inline;">9. Limitation of Liability </h3></span>
+            <br>
+            <br>
+            <div class="about-bot-txt">
+        <ul style="gap: 10px; display: grid;">
+            <li>Fooyes UK is not responsible for indirect, incidental, or consequential damages arising
+from the use of our services.
+</li>
+            <li>We do not guarantee uninterrupted or error-free access to our website.</li>
+        </ul>
+        <span><h3 style="display: inline;">9. Changes to Terms and Conditions</h3></span>
+            <br>
+            <br>
+            <div class="about-bot-txt">
+        <ul style="gap: 10px; display: grid;">
+            <li>We reserve the right to update these Terms and Conditions at any time. Continued use
+            of our website signifies acceptance of any changes.</li>
+        </ul>
+        <span><h3 style="display: inline;">10. Contact Us
+        </h3></span>
+            <br>
+            <br>
+            <div class="about-bot-txt">
+        <ul style="gap: 10px; display: grid;">
+            <li>For any questions regarding these Terms and Conditions, please contact us at (877-67-88-99)</li>
+        </ul>
+        </div>
         </div>
     </div>
 </section>
