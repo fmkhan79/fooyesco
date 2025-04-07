6d93c8f92a5081331ccb2855774f1b4d96098107
rafaykui <website25developer@gmail.com>
2025-04-07 19:15:31 +0500

    rf-privacy

--- application/views/frontend/default/privacy_policy/index.php
+++ application/views/frontend/default/privacy_policy/index.php
@@ -4,80 +4,98 @@
 <section class="detail-wbox mt-4 mb-2">
     <div class="container bg-white text-dark border border-light">
 
-        <h1 style="font-size:50px; font-weight:700;" class="mt-5 mb-5 text-dark"><?php echo site_phrase('privacy_policy', true) ?></h1>
+        <h1 style="font-size:50px; font-weight:700;" class="mt-5 mb-5 text-dark"><span class="text-privacy">Privacy</span>  <span class="text-policy">Policy</span>
 
-        <div class="about-bot-txt my-5">
-            <p>At Fooyes UK, we value your privacy and are committed to protecting your personal data. This
-                Privacy Policy outlines how we collect, use, store, and safeguard your information when you
-                visit our website or interact with our services.
+        <div class="about-bot-txt my-4 ">
+            <p><span class="txt-fo " style="font-weight:700;">At Fooyes UK,</span><p style="font-weight:500;" > we value your privacy and are committed to protecting your personal data. ThisPrivacy Policy outlines how we collect, use, store, and safeguard your information when you visit our website or interact with our services.</p>
             </p>
             <br>
-            <span><h3 style="display: inline;">1. Information We Collect <span style="font-weight:300; font-size:30px;">We may collect and process the following types of personal data:
-            </span></h3></span>
+            <span class="txt-up-fo">Information We Collect </span> 
+            <br>
+            <span class="text-fo" >We may collect and process the following types of personal data:</span >
             <br>
             <br>
             <div class="about-bot-txt">
         <ul style="gap: 10px; display: grid;">
-            <li> <b>Personal Information: </b>Name, email address, phone number, and delivery address.</li>
-            <li> <b>Payment Information:</b>  Billing details and payment method (processed securely through third-party providers).</li>
-            <li> <b>Usage Data:</b> Information about how you use our website, including browsing history and preferences.</li>
-            <li> <b> Cookies and Tracking Technologies:</b> To improve user experience and analyze website traffic.</li>
+             <p class="head-1" >Personal Information: </p ><p style="font-weight:500;"> Name, email address, phone number, and delivery address.</p>
+             <p class="head-1" >Payment Information: </p ><p  style="font-weight:500;"> Billing details and payment method (processed securely through third-party providers).</p>
+             <p class="head-1" >Usage Data: </p ><p  style="font-weight:500;">Information about how you use our website, including browsing history and preferences.</p>
+             <p class="head-1" >Cookies and Tracking Technologies: </p ><p  style="font-weight:500;">  To improve user experience and analyze website traffic.</p>
+            <!-- <li> <p class="head-1" >Personal Information: </p ><p> Name, email address, phone number, and delivery address.</p></li > -->
+            <!-- <li> <b>Payment Information:</b>  Billing details and payment method (processed securely through third-party providers).</li> -->
+            <!-- <li> <b>Usage Data:</b> Information about how you use our website, including browsing history and preferences.</li> -->
+            <!-- <li> <b> Cookies and Tracking Technologies:</b> To improve user experience and analyze website traffic.</li> -->
         </ul>
-            <span><h3 style="display: inline;">2. How We Use Your Information <span style="font-weight:300; font-size:30px;">We use your personal data for the following purposes:
-            </span></h3></span>
+            <!-- <span><h3 style="display: inline;">2. How We Use Your Information <span>We use your personal data for the following purposes:
+            </span></h3></span> -->
+            <span class="txt-up-fo">How We Use Your Information </span> 
             <br>
+            <span class="text-fo" >We use your personal data for the following purposes:</span >
             <br>
             <div class="about-bot-txt">
         <ul style="gap: 10px; display: grid;">
-            <li>To process and fulfill orders</li>
-            <li>To communicate with you about your purchases and provide customer support.</li>
+        <p class="head-1" ><p style="font-weight:500;"> To process and fulfill orders</p>
+        <p class="head-1" ><p style="font-weight:500;"> To communicate with you about your purchases and provide customer support.</p>
+        <p class="head-1" ><p style="font-weight:500;"> To send promotional offers and marketing communications (with your consent).</p>
+        <p class="head-1" ><p style="font-weight:500;"> To comply with legal obligations and prevent fraudulent activity.</p>
+            <!-- <li>To process and fulfill orders</li> -->
+            <!-- <li>To communicate with you about your purchases and provide customer support.</li>
             <li>To improve our website, products, and services</li>
             <li>To send promotional offers and marketing communications (with your consent).</li>
-            <li>To comply with legal obligations and prevent fraudulent activity.</li>
+            <li>To comply with legal obligations and prevent fraudulent activity.</li> -->
         </ul>
-            <span><h3 style="display: inline;">3. Data Sharing and Security</h3></span>
-            <br>
+            <span class="txt-up-fo">Data Sharing and Security </span> 
             <br>
             <div class="about-bot-txt">
-        <ul style="gap: 10px; display: grid;">
-            <li>We do not sell or rent your personal data to third parties</li>
+        <ul style="gap: 10px;display: grid;">
+        <p class="head-1" ><p style="font-weight:500;"> We do not sell or rent your personal data to third parties</p>
+        <p class="head-1" ><p style="font-weight:500;"> Your information may be shared with trusted service providers (e.g., payment processors and delivery services) for operational purposes.</p>
+        <p class="head-1" ><p style="font-weight:500;"> We implement strict security measures to protect your data from unauthorized access or breaches</p>
+            <!-- <li>We do not sell or rent your personal data to third parties</li>
             <li>Your information may be shared with trusted service providers (e.g., payment processors and delivery services) for operational purposes.</li>
-            <li>We implement strict security measures to protect your data from unauthorized access or breaches</li>
+            <li>We implement strict security measures to protect your data from unauthorized access or breaches</li> -->
         </ul>
-        <span><h3 style="display: inline;">4. Your Rights <span style="font-weight:300; font-size:30px;">You have the right to:
-            </span></h3></span>
-            <br>
+        <span class="txt-up-fo">Your Rights  </span> <span class="red-fo" >You have the right to:</span >
+        <!-- <span><h3 style="display: inline;">4. Your Rights <span style="font-weight:300; font-size:30px;">You have the right to:
+            </span></h3></span> -->
             <br>
             <div class="about-bot-txt">
         <ul style="gap: 10px; display: grid;">
-            <li>Access, update, or delete your personal information.</li>
+        <p class="head-1" ><p style="font-weight:500;"> Access, update, or delete your personal information.</p>
+        <p class="head-1" ><p style="font-weight:500;"> Opt-out of marketing communications at any time.</p>
+        <p class="head-1" ><p style="font-weight:500;"> Request details about the data we hold about you.</p>
+        <p class="head-1" ><p style="font-weight:500;"> To send promotional offers and marketing communications (with your consent).</p>
+        <p class="head-1" ><p style="font-weight:500;"> Lodge a complaint with a data protection authority if you believe your rights have been violated.</p>
+            <!-- <li>Access, update, or delete your personal information.</li>
             <li>Opt-out of marketing communications at any time.</li>
             <li> Request details about the data we hold about you.</li>
             <li>To send promotional offers and marketing communications (with your consent).</li>
-            <li>Lodge a complaint with a data protection authority if you believe your rights have been violated.</li>
+            <li>Lodge a complaint with a data protection authority if you believe your rights have been violated.</li> -->
         </ul>
-        <span><h3 style="display: inline;">5. Cookies Policy</h3></span>
-            <br>
+        <span class="txt-up-fo">Cookies Policy </span> 
             <br>
             <div class="about-bot-txt">
         <ul style="gap: 10px; display: grid;">
-            <li>We use cookies to enhance your browsing experience. You can manage your cookie
-            preferences through your browser settings.</li>
+        <p class="head-1" ><p style="font-weight:500;"> We use cookies to enhance your browsing experience. You can manage your cookie
+        preferences through your browser settings.</p>
+
+            <!-- <li >We use cookies to enhance your browsing experience. You can manage your cookie
+            preferences through your browser settings.</li> -->
         </ul>
-        <span><h3 style="display: inline;">6. Updates to This Policy</h3></span>
-            <br>
+        <span class="txt-up-fo">Updates to This Policy </span>
             <br>
             <div class="about-bot-txt">
         <ul style="gap: 10px; display: grid;">
-            <li>We may update this Privacy Policy from time to time. Any changes will be posted on this page with an updated effective date.</li>
+        <p class="head-1" ><p style="font-weight:500;"> We may update this Privacy Policy from time to time. Any changes will be posted on this page with an updated effective date.</p>
         </ul>
-        <span><h3 style="display: inline;">7. Contact Us</h3></span>
-            <br>
+        <span class="txt-up-fo"> Contact Us</></span>
             <br>
             <div class="about-bot-txt">
         <ul style="gap: 10px; display: grid;">
-            <li>If you have any questions about our Privacy Policy or how we handle your data, please
-            contact us at Contact Information].</li>
+            <p class="head-1"><p style="font-weight:500;"> If you have any questions about our Privacy Policy or how we handle your data, please
+            contact us at Contact Information].
+            <!-- <li>If you have any questions about our Privacy Policy or how we handle your data, please
+            contact us at Contact Information].</li> -->
         </ul>
         </div>
         </div>
