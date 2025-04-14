c050a9ca507f16e598c716af8472df04ae726a55
Mutayyab <website25developer@gmail.com>
2025-04-14 17:01:45 +0500

    testing

--- application/views/frontend/default/restaurant/scripts/index-script.php
+++ application/views/frontend/default/restaurant/scripts/index-script.php
@@ -846,8 +846,8 @@ $.ajax({
             // Get both buttons
         var guestButtons = document.querySelectorAll(".guestCheckoutBtn");
         var checkoutButton = document.querySelectorAll(".CheckoutBtn");
-        var guestcheckoutButtonmobile = document.getElementById(".guestCheckoutBtnmobile");
-        var checkoutButtonmobile = document.getElementById(".CheckoutBtnmobile");
+        var guestcheckoutButtonmobile = document.getElementById("guestCheckoutBtnmobile");
+        var checkoutButtonmobile = document.getElementById("CheckoutBtnmobile");
 
         // Check if elements are not null before modifying them
         if (guestButtons) {
