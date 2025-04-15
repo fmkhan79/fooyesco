c1ab8578c42a83d5f9560e2c081931fae850fd51
Mutayyab <website25developer@gmail.com>
2025-04-15 19:28:14 +0500

    print_recipt-changed

--- application/views/backend/owner/orders/print_receipt.php
+++ application/views/backend/owner/orders/print_receipt.php
@@ -96,7 +96,7 @@
     }
 
     echo "<h3 style='margin:0px;'>" . $order_details["customer_name"] . "</h3>";
-    echo "<h3 style='margin:0px;'>" . $address["additional_address"] . ", " . $address["street"] . "</h3>";
+    echo "<h3 style='margin:0px;'>" . $address["additional_address"]  . $address["street"] . "</h3>";
 ?>
 
         
