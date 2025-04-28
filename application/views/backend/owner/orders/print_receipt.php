bcd0c639c4180dcd4f47c763d46626b3b865a754
Mutayyab <website25developer@gmail.com>
2025-04-28 13:59:12 +0500

    print_recipt update

--- application/views/backend/owner/orders/print_receipt.php
+++ application/views/backend/owner/orders/print_receipt.php
@@ -91,13 +91,17 @@
 
      
      <?php 
-    if ($order_details['order_type'] == "delivery") {
-        echo "<h3>DELIVERY</h3>";
-    }
+if ($order_details['order_type'] == "delivery") {
+    echo "<h3>DELIVERY</h3>";
+}
 
-    echo "<h3 style='margin:0px;'>" . $order_details["customer_name"] . "</h3>";
-    echo "<h3 style='margin:0px;'>" . $address["street"] . ", ". str_replace(", March, UK", "", $address["additional_address"]) . " - ".  $address["zip_code"] . "</h3>";
+echo "<h3 style='margin:0px;'>" . $order_details["customer_name"] . "</h3>";
+
+if ($order_details['order_type'] != "pickup") {
+    echo "<h3 style='margin:0px;'>" . $address["street"] . ", " . str_replace(", March, UK", "", $address["additional_address"]) . " - " . $address["zip_code"] . "</h3>";
+}
 ?>
+
 
         
         <?php  
