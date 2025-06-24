74cd495de6049a0fad8a6c0c3baaa4ce8037a467
web25-svg <176150828+web25-svg@users.noreply.github.com>
2025-06-24 15:13:44 +0500

    styling

--- application/views/backend/admin/report/styles/index-style.php
+++ application/views/backend/admin/report/styles/index-style.php
@@ -7,24 +7,26 @@
 <link rel="stylesheet" href="<?php echo base_url('assets/backend/'); ?>plugins/select2/css/select2.min.css">
 
 <style>
-
-#bulk-paid-btn{
-    background-color: #f54748 !important;
-    border-radius: 2rem;
-    border: none;
-}
-.page-link{
-    color: black !important;
-}
-.page-item:has(.active) {
-    background-color: #ffc107 !important;
-}
-
-#bulk-unpaid-btn{
-    background-color: grey !important;
-    border-radius: 2rem;
-    border: none;
-}
-
-
+    #bulk-paid-btn {
+        background-color: #f54748 !important;
+        border-radius: 2rem;
+        border: none;
+    }
+    .page-link {
+        color: black !important;
+    }
+    .page-item:has(.active) {
+        background-color: #ffc107 !important;
+    }
+    #bulk-unpaid-btn {
+        background-color: grey !important;
+        border-radius: 2rem;
+        border: none;
+    }
+    .dt-buttons .buttons-csv {
+        background-color: #f54748 !important;
+        border-radius: 2rem !important;
+        border: none !important;
+        color: #fff !important;
+    }
 </style>
