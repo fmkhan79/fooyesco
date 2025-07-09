22c64b18d2f06bede03e97c15bafcf1c68a6bdde
ahmed <website25developer@gmail.com>
2025-07-09 13:12:24 +0500

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
 $route['default_controller'] = 'site';
@@ -6,28 +6,21 @@ $route['404_override'] = '';
 $route['login'] = 'auth/index';
 $route['logout'] = 'auth/logout';
 $route['translate_uri_dashes'] = FALSE;
+$route['site/contact-us'] = 'site/contact_us';
+$route['site/about-us'] = 'site/about_us';
+$route['site/privacy-policy'] = 'site/privacy_policy';
+$route['site/terms-and-conditions'] = 'site/terms_and_conditions';
+$route['site/become-a-partner'] = 'site/become_a_partner';
+$route['site/terms-of-use'] = 'site/terms_of_use';
 
-$route['contact_us'] = 'site/contact_us';
-$route['about_us'] = 'site/about_us';
-$route['privacy-policy'] = 'site/privacy_policy';
-$route['terms-and-conditions'] = 'site/terms_and_conditions';
-$route['become-a-partner'] = 'site/become_a_partner';
-$route['terms-of-use'] = 'site/terms_of_use';
+$route['chilli-hut-march'] = 'site/restaurant/chilli-hut-march/3';
+$route['commision'] = 'report/sales_summary';
+
+
 
 $route['refundrequest'] = 'RefundRequest';
 $route['refundrequest/(:any)'] = 'RefundRequest/$1';
 $route['refundrequest/(:any)/(:any)'] = 'RefundRequest/$1/$2';
 $route['submissions'] = 'submissions';
-// $route['submissions/(:any)'] = 'Submissions/$1';
-
-$route['chilli-hut-march'] = 'site/restaurant/chilli-hut-march/3';
-$route['commision'] = 'report/sales_summary';
-$route['restaurants/popular'] = 'site/restaurants/popular';
-$orute['site/restaurants/recent'] = 'site/restaurants/recent';
-$route['restaurants/filter?query=&latitude_1=&longitude_1='] = 'site/restaurants/filter?query=&latitude_1=&longitude_1=';
-$route['restaurant/pizza-king/13'] = 'site/restaurant/pizza-king/13';
-$route['restaurant/the-spice-bank/11'] = 'site/restaurant/the-spice-bank/11';
-$route['restaurant/chilli-hut-march/3'] = 'site/restaurant/chilli-hut-march/3';
-
-
+// $route['check'] = 'orders/check_new_orders';
 // $route['check'] = 'orders/check_new_orders';
