444cc7f9b733fdc954b2d2245de574a922f97028
web25-svg <website25developer@gmail.com>
2025-08-15 19:27:01 +0500

    a fixess

--- application/views/frontend/default/terms_and_conditions/index.php
+++ application/views/frontend/default/terms_and_conditions/index.php
@@ -4,17 +4,8 @@
 
 <?php 
 $host = get_subdomain();
-if($host != 'fooyes' || $host != 'staging'):
+if($host == 'fooyes' || $host == 'staging'):
 ?>
-<section class="detail-wbox mt-4 mb-2">
-    <div class="container bg-white text-dark border border-light">
-        <h1 style="font-size:40px; font-weight:600;" class="text-dark mb-5"><span class="text-privacy">Terms &</span>  <span class="text-policy">Conditions</span></h1>
-
-        <?= $restaurant_details['terms_conditions'] ?>
-    </div>
-</section>
-<?php else: ?>
-
 <section class="detail-wbox mt-4 mb-2 " >
     <div class="container bg-white text-dark border border-light">
 
@@ -112,6 +103,15 @@ if($host != 'fooyes' || $host != 'staging'):
         </div>
     </div>
 </section>
+<?php else: ?>
+<section class="detail-wbox mt-4 mb-2">
+    <div class="container bg-white text-dark border border-light">
+        <h1 style="font-size:40px; font-weight:600;" class="text-dark mb-5"><span class="text-privacy">Terms &</span>  <span class="text-policy">Conditions</span></h1>
+
+        <?= $restaurant_details['terms_conditions'] ?>
+    </div>
+</section>
+
 <?php endif; ?>
 
 
