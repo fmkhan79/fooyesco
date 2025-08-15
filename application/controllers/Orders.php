5498cb5982c07a193f77b3a4d0296d760f64cbed
web25-svg <website25developer@gmail.com>
2025-08-15 15:21:39 +0500

    done toggle

--- application/controllers/Orders.php
+++ application/controllers/Orders.php
@@ -433,6 +433,12 @@ class Orders extends Authorization
 
         redirect(site_url('report/index'));
     }
+
+    public function set_test_toggle()
+    {
+        $this->session->set_userdata('show_test_orders', $this->input->post('show_test_orders'));
+    }
+
 }
 
 /* End of file Orders.php */
