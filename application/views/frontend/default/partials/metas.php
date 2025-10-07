85a285e73a95943ab56a2025d4c044341fe43b4e
ahmed <website25developer@gmail.com>
2025-10-07 15:25:52 +0500

    chnages

--- application/views/frontend/default/partials/metas.php
+++ application/views/frontend/default/partials/metas.php
@@ -14,5 +14,23 @@ if ($page_name == "restaurant/index") : ?>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- PAGE TITLE ARE SAFE. SINCE THEY ARE BEING GENERATED INSIDE THE SYSTEM -->
+
+<?php 
+$restaurant_ids = $this->cart_model->get_restaurant_ids();
+
+if (count($restaurant_ids) > 0) {
+    $restaurant_details = $this->restaurant_model->get_by_id($restaurant_ids[0]);
+}
+$isFooyes = false;
+$host = get_subdomain();
+
+if($host === 'fooyes' || $host === 'staging')
+    $isFooyes=true;
+?>
+
+<?php if($isFooyes): ?>
 <title><?php echo htmlspecialchars($page_title); ?> | <?php echo sanitize(get_system_settings('system_title')); ?></title>
+<?php else: ?>
+<title><?php echo htmlspecialchars($page_title); ?> | <?php echo $restaurant_details['name']; ?></title>
+<?php endif; ?>
 <link rel="shortcut icon" href="<?php echo base_url('uploads/system/' . get_website_settings('favicon')); ?>">
