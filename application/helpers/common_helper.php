19f22101bee4a3c41f2048d4eee0d6ae39df1dd1
ahmed <website25developer@gmail.com>
2026-02-09 18:22:24 +0500

    Update common_helper.php

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -187,8 +187,8 @@ if (!function_exists('frontend')) {
 if (!function_exists('slugify')) {
     function slugify($text)
     {
-        $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
-        $text = trim($text, '-');
+        $text = preg_replace('~[^\\pL\d]+~u', '', $text);
+        $text = trim($text, '');
         $text = strtolower($text);
         //$text = preg_replace('~[^-\w]+~', '', $text);
         if (empty($text))
