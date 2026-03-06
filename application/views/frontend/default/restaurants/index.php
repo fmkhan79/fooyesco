ff99ece788c4fae518a4dd13635b63f1dfa462fd
ahmed <website25developer@gmail.com>
2026-03-07 00:18:08 +0500

    again

--- application/views/frontend/default/restaurants/index.php
+++ application/views/frontend/default/restaurants/index.php
@@ -296,8 +296,8 @@ $ctaLink = $this->order_model->getSetting('ctaLink');
                 <div class="single-item-order">
                     <div class="order-box d-md-flex align-items-center mb-4 justify-content-around">
                         <div class="order-img-box main-img m-0">
-                           <a href="<?php echo site_url(sanitize($restaurant['slug'])); ?>">
-                              
+                            <a
+                                href="<?php echo site_url('site/restaurant/' . sanitize(rawurlencode($restaurant['slug'])) . '/' . sanitize($restaurant['id'])); ?>">
                                 <img src="<?php echo base_url('uploads/restaurant/thumbnail/' . sanitize($restaurant['thumbnail'])); ?>"
                                     alt="#">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="250" height="250" viewBox="0 0 250 250"
@@ -318,7 +318,8 @@ $ctaLink = $this->order_model->getSetting('ctaLink');
                         </div>
 
                         <div class="order-detail">
-                            <h4><a href="<?php echo site_url(sanitize($restaurant['slug'])); ?>">
+                            <h4><a
+                                    href="<?php echo site_url('site/restaurant/' . sanitize(rawurlencode($restaurant['slug'])) . '/' . sanitize($restaurant['id'])); ?>">
                                     <?php echo sanitize($restaurant['name']); ?>
                                 </a></h4>
                             <div class="review-grid d-flex justify-content-around align-items-center">
