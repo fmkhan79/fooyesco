cd296493ea7a4a91f782aaf0fdeb197e9d672fad
web25-svg <176150828+web25-svg@users.noreply.github.com>
2026-06-30 15:59:19 +0500

    Update edit.php

--- application/views/backend/owner/menu/edit.php
+++ application/views/backend/owner/menu/edit.php
@@ -38,14 +38,15 @@
                                         </select>
                                     </div>
 
-   <div class="form-group">
+   <div class="form-group">                               
     <label>Sub Category</label>
-
     <select class="form-control select2 w-100" name="sub_category_id" id="subcategory_id">
-
+    <?php if ($menu_data['subcategory_id'] == null) : ?>
+        <option value="" selected disabled>Select Sub Category</option>
+    <?php endif; ?>
         <?php foreach ($subcategories as $sub) : ?>
             <option value="<?php echo $sub['id']; ?>"
-                <?php echo ($menu_data['subcategory_id'] == $sub['id']) ? 'selected' : ''; ?>>
+                <?php echo ($menu_data['sub_category_id'] == $sub['id']) ? 'selected' : ''; ?>>
 
                 <?php echo $sub['name']; ?>
             </option>
